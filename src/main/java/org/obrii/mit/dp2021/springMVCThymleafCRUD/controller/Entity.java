11c679432a6f1dff727b3609a68b77c8ad5487d9
margosha <margosha@air-margosha.dlink>
2021-05-01 10:48:50 +0300

    lab7

---
+++ src/main/java/org/obrii/mit/dp2021/springMVCThymleafCRUD/controller/Entity.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.obrii.mit.dp2021.springMVCThymleafCRUD.controller;
+
+/**
+ *
+ * @author margosha
+ */
+public class Entity {
+    
+    private  long id;
+    private  String name;
+    private  String email;
+
+    private int age;
+
+    public Entity() {
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
